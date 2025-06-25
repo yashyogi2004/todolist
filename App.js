6fdcf679263a97bb062baac20e19ea135c47e7b0
yashyogi2004 <yashyog2004@gmail.com>
2025-06-25 08:15:13 +0530

    morning

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const dotenv = require('dotenv');
+const path =require('path');
 const {connection} = require('./db/connection');
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
@@ -8,8 +9,9 @@ const jwt = require('jsonwebtoken');
 const isLoggedin = require('./middlewares/isLoggedin');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 dotenv.config();
 connection();
 const userModel = require('./model/user');
